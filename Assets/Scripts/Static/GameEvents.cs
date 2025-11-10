ede529c6e2ba208669e1a8edcc7af781a529f7b1
Arhilag <156521644+ALexFotoN@users.noreply.github.com>
2025-11-11 00:47:37 +0300

    Clear folders

--- Assets/Scripts/Static/GameEvents.cs
+++ Assets/Scripts/Static/GameEvents.cs
@@ -6,4 +6,6 @@ public static class GameEvents
     public static Action OnDestructionModeEntered;
 
     public static Action<BuildingData> OnBuildModeEntered;
+
+    public static Action<Vector2> OnMouseUp;
 }
